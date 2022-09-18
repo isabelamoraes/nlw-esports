df4697cd25175ccf924c636b9a2420d189ff1cfb
Isabela Moraes <isabelagmoraes@hotmail.com>
2022-09-18 00:28:30 -0300

    feat: upload to git

---
+++ mobile/src/components/GameCard/index.tsx
@@ -0,0 +1,47 @@
+import { LinearGradient } from 'expo-linear-gradient';
+import {
+  ImageBackground,
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps
+} from 'react-native';
+
+import { THEME } from '../../theme';
+import { styles } from './styles';
+
+export interface GameCardProps {
+  id: string;
+  title: string;
+  _count: {
+    ads: number;
+  }
+  bannerUrl: string;
+}
+
+interface Props extends TouchableOpacityProps{
+  data: GameCardProps;
+}
+
+export function GameCard({ data, ...rest }: Props){
+  return (
+    <TouchableOpacity style={styles.container} {...rest}>
+      <ImageBackground 
+        source={{uri: data.bannerUrl}} 
+        style={styles.cover} 
+      >
+        <LinearGradient
+          colors={THEME.COLORS.FOOTER}
+          style={styles.footer}
+        >
+          <Text style={styles.name}>
+            {data.title}
+          </Text>
+
+          <Text style={styles.ads}>
+            {data._count.ads} anúncios
+          </Text>
+        </LinearGradient>
+      </ImageBackground>
+    </TouchableOpacity>
+  );
+}
---
+++ web/src/App.tsx
@@ -0,0 +1,57 @@
+import * as Dialog from '@radix-ui/react-dialog';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+import { CreateAdBanner } from './components/CreateAdBanner';
+import { CreateAdModal } from './components/CreateAdModal';
+import { GameBanner } from './components/GameBanner';
+
+import logoSvg from './assets/logo.svg';
+import './styles/main.css';
+
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  }
+}
+
+function App() {
+  const [games, setGames] = useState<Game[]>([]);
+
+  useEffect(() => {
+    axios('http://localhost:3333/games').then(response => {
+      setGames(response.data)
+    });
+  }, []);
+
+  return (
+    <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
+      <img src={logoSvg} />
+
+      <h1 className='text-6xl text-white font-black mt- 20'>Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui.</h1>
+
+      <div className='grid grid-cols-6 gap-6 mt-16'>
+        {games.map(game => {
+          return (
+            <GameBanner 
+              key={game.id}
+              bannerUrl={game.bannerUrl} 
+              title={game.title}
+              adsCount={game._count.ads} 
+            />
+          )
+        })}
+      </div>
+
+      <Dialog.Root>
+        <CreateAdBanner />
+        <CreateAdModal />
+      </Dialog.Root>
+    </div>
+  )
+}
+
+export default App
